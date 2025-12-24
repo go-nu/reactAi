69f38eb49358ab636c5f32f32556f9dd20b8d9c5
gonu <rjsdnrh697@naver.com>
2025-12-24 18:13:21 +0900

    1224 4차 실험 진행 중

--- codi_on/ml/pipeline/config.py
+++ codi_on/ml/pipeline/config.py
@@ -2,20 +2,20 @@ TRAIN_CONFIG = {
     "input_dim": 11,
     "use_ap": False,
     "hidden_dims": [32, 16],
-    "activation": "relu",
+    "activation": "tanh",
     "dropout": 0.1,
 
     "batch_size": 32,
-    "epochs": 200,
+    "epochs": 100,
 
     "optimizer": "adam",
-    "learning_rate": 5e-3,
+    "learning_rate": 1e-3,
 
     "loss": "mse",
 
     # early stopping
-    "early_stopping": False,
+    "early_stopping": True,
     "es_patience": 20, # epoch 기준
-    "es_min_delta": 2e-4, # 최소 변화량
+    "es_min_delta": 1e-4, # 최소 변화량
     "es_monitor": "loss", # loss | acc
 }
